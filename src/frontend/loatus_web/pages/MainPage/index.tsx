5230c60da7b95c91b9e719a51e3e9abc2e61e8bf
BiteSnail <rkwoal216@gmail.com>
2023-01-26 20:41:38 +0900

    [feature] 테스트 페이지 제작

--- src/frontend/loatus_web/pages/MainPage/index.tsx
+++ src/frontend/loatus_web/pages/MainPage/index.tsx
@@ -1,14 +1,32 @@
 import React from "react";
 
 const onclick = () => {
+        fetch("http://192.168.115.15:8080/oauth2/authorization/naver")
+        .then(response => {
+            console.log(response.text())
+        })
+        .catch(error => {
+            console.log(error)
+        })
     return;
+}
+
+const onclick2 = () => {
+    fetch("http://192.168.115.15:8080/api/hello")
+        .then(response => {
+            console.log(response.text())
+        })
+        .catch(error => {
+            console.log(error)
+        })
 }
 
 export const MainPage = () => {
     return (
         <div>
             <h1>Loatus</h1>
-            <button onClick={onclick}>테스트용 버튼</button>
+            <button onClick={onclick}>네이버 버튼</button>
+            <button onClick={onclick2}>테스트용 버튼</button>
         </div>
     )
 }
