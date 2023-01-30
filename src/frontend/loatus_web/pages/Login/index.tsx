09226bfdb48cc63e27e8680d650cef9d5115841e
BiteSnail <rkwoal216@gmail.com>
2023-01-30 21:07:43 +0900

    [feature] Login 페이지 제작

---
+++ src/frontend/loatus_web/pages/Login/index.tsx
@@ -0,0 +1,118 @@
+import React, { useCallback, useState } from 'react';
+import useInput from '@hooks/useInput';
+import axios from 'axios';
+import { Button, Header, Horizon, Hr, Input, Page, PageHead, Root, SignIn } from '@pages/Login/styles';
+import { Link, Navigate } from 'react-router-dom';
+import { IToken, IUser } from '@typings/db';
+import usePropSWR from '@hooks/usePropSWR';
+import useSWR from 'swr';
+import Signup from '@pages/Signup';
+
+const Login = () => {
+  const [email, onChangeEmail, setEmail] = useInput('');
+  const [token, setToken] = useState<IToken>({
+    accessToken: '',
+    grantType: '',
+    refreshToken: '',
+    username: '',
+  });
+  const [password, onChangePassword, setPassword] = useInput('');
+  const fetcher = (url: string) =>
+    axios
+      .get(url, {
+        withCredentials: true,
+        headers: {
+          Authorization: 'Bearer ' + token.accessToken,
+        },
+      })
+      .then((response) => response.data);
+  const {
+    data: userData,
+    error,
+    mutate,
+  } = useSWR<IUser | undefined | null>('/auth/my', fetcher, {
+    onErrorRetry: (error) => {
+      if (error.status === 504) return;
+    },
+  });
+
+  const onSubmitLogin = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      axios
+        .post(
+          `/auth/login`,
+          {
+            email: { email },
+            password: { password },
+          },
+          {
+            withCredentials: true,
+          },
+        )
+        .then((response) => {
+          setToken((prev) => response?.data);
+          mutate();
+          setEmail('');
+        })
+        .catch((error) => {
+          console.dir(error);
+        })
+        .finally(() => setPassword(''));
+    },
+    [email, password],
+  );
+
+  if (userData) {
+    return <Navigate to="/Mainpage" replace />;
+  }
+
+  return (
+    <Root>
+      <Header>
+        <div className="left-col"></div>
+        <div className="center-col">
+          <h1 style={{ textAlign: 'center' }}>Loatus</h1>
+        </div>
+        <div className="right-col">
+          <div
+            style={{
+              color: 'rgba(var(--sk_foreground_max_solid,97,96,97),1)',
+              fontSize: '13px',
+              paddingRight: '40px',
+              textAlign: 'right',
+            }}
+          >
+            <div>Loatus가 처음이신가요?</div>
+            <Link to="/signup">계정 생성</Link>
+          </div>
+        </div>
+      </Header>
+      <Page>
+        <PageHead>이메일로 로그인해 보세요.</PageHead>
+        <SignIn>
+          <Button>네이버를 사용하여 로그인</Button>
+          <Button>카카오를 사용하여 로그인</Button>
+          <Horizon>
+            <Hr />
+            <div style={{ padding: '0 20px' }}>또는</div>
+            <Hr />
+          </Horizon>
+          <form onSubmit={onSubmitLogin}>
+            <label id="email-label">
+              <span>이메일</span>
+              <Input placeholder={'name@email.com'} type="email" value={email} onChange={onChangeEmail} />
+            </label>
+            <label id="password-label">
+              <span>비밀번호</span>
+              <Input type="password" value={password} onChange={onChangePassword} />
+            </label>
+            <Button type="submit">로그인</Button>
+          </form>
+        </SignIn>
+      </Page>
+    </Root>
+  );
+};
+
+export default Login;
---
+++ src/frontend/loatus_web/pages/Login/styles.tsx
@@ -0,0 +1,112 @@
+import styled from '@emotion/styled';
+
+export const Header = styled.header`
+  align-items: center;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  padding: 48px 0 40px;
+  width: 100%;
+`;
+
+export const Root = styled.div`
+  align-items: center;
+  background-color: rgba(var(--sk_primary_background, 255, 255, 255), 1);
+  display: flex;
+  flex-direction: column;
+  min-height: 100%;
+`;
+
+export const Button = styled.button`
+  -webkit-tap-highlight-color: transparent;
+  align-items: center;
+  -webkit-appearance: none;
+  width: 100%;
+  max-width: 100%;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  outline: none;
+  position: relative;
+  text-align: center;
+  -webkit-user-select: none;
+  user-select: none;
+  white-space: nowrap;
+  box-shadow: none;
+  transition: all 80ms linear;
+  font-size: 18px;
+  font-weight: 700;
+  height: 44px;
+  min-width: 96px;
+  background-color: #fff;
+  border: 2.5px solid #000000;
+  color: #000000;
+  margin-bottom: 20px;
+`;
+
+export const Hr = styled.hr`
+  border: none;
+  border-top: 1px solid #ddd;
+  clear: both;
+  flex-grow: 1;
+  margin: 0;
+`;
+
+export const Horizon = styled.div`
+  align-items: center;
+  display: flex;
+  margin-top: 24px;
+  margin-bottom: 24px;
+`;
+
+export const Page = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  flex-shrink: 0;
+  max-width: 800px;
+  width: 100%;
+  box-sizing: inherit;
+`;
+
+export const PageHead = styled.h1`
+  font-family: Slack-Larsseit, Helvetica Neue, Helvetica, Segoe UI, Tahoma, Arial, sans-serif;
+  font-size: 60px;
+  font-weight: 700;
+  letter-spacing: -0.75px;
+  line-height: 46px;
+  margin-bottom: 10px;
+  max-width: 700px;
+  text-align: center;
+  margin-block-start: 0.67em;
+  margin-block-end: 0.67em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+`;
+
+export const Input = styled.input`
+  padding: 11px 12px 13px;
+  height: 44px;
+  font-size: 18px;
+  line-height: 1.33333333;
+  width: 100%;
+  border-radius: 4px;
+  border: 1px solid rgba(29, 28, 29, 0.3);
+  transition: border 80ms ease-out, box-shadow 80ms ease-out;
+  box-sizing: border-box;
+  margin: 0 0 20px;
+`;
+
+export const SignIn = styled.div`
+  font-size: 15px;
+  line-height: 1.46668;
+  font-weight: initial;
+  color: #1d1c1d;
+  font-variant-ligatures: common-ligatures;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  display: block;
+  width: 400px;
+`;
