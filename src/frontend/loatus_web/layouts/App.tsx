1544ae5690dfbed089df41ad34df9758c3393cd8
BiteSnail <rkwoal216@gmail.com>
2023-01-30 21:06:42 +0900

    [feature] Route구조 변경

--- src/frontend/loatus_web/layouts/App.tsx
+++ src/frontend/loatus_web/layouts/App.tsx
@@ -1,13 +1,18 @@
 import React, { FC } from 'react';
 import { Routes, Route, Navigate, BrowserRouter } from 'react-router-dom';
-import {MainPage} from "@pages/MainPage";
+import { MainPage } from '@pages/MainPage';
+import Login from '@pages/Login';
+import Signup from '@pages/Signup';
 
 const App: FC = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<MainPage/>}/>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/Mainpage" element={<MainPage />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="*" element={<Navigate to={'/login'} replace />} />
+    </Routes>
+  );
 };
 
 export default App;
