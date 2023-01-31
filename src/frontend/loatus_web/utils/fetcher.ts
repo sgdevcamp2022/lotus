2c790bcef1b4808803e76df0864efa61c5f30768
BiteSnail <rkwoal216@gmail.com>
2023-01-31 11:42:22 +0900

    [feature] 쿠키를 이용한 userData 불러오기 fetcher 제작

--- src/frontend/loatus_web/utils/fetcher.ts
+++ src/frontend/loatus_web/utils/fetcher.ts
@@ -1,10 +1,16 @@
 import axios from 'axios';
+import { useCookies } from 'react-cookie';
 
-const fetcher = (url: string) =>
-  axios
+const fetcher = (url: string) => {
+  const [cookie] = useCookies(['accessToken']);
+  return axios
     .get(url, {
       withCredentials: true,
+      headers: {
+        Authorization: 'Bearer ' + cookie,
+      },
     })
     .then((response) => response.data);
+};
 
 export default fetcher;
