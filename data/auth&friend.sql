c178eeeb5c13fc265e25ad577b3a824ba43ef3be
the-groot <sangwoo0795@naver.com>
2023-02-05 10:59:25 +0900

    docs: auth&friend 테이블 sql문 생성

---
+++ data/auth&friend.sql
@@ -0,0 +1,50 @@
+USE auth;
+
+CREATE TABLE user (
+user_id bigint AUTO_INCREMENT,
+activated bit(1),
+auth varchar(50),
+character_name varchar(50),
+email varchar(50) NOT NULL,
+nickname varchar(50),
+password varchar(100),
+profile_image varchar(100),
+provider varchar(10),
+stove_no varchar(15),
+
+PRIMARY KEY(user_id)
+);
+
+INSERT INTO user(email, password, nickname, activated, auth, provider)
+VALUES('admin', '$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 'admin', 1, 'ROLE_ADMIN', 'LOCAL'),
+('user', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'user', 1, 'ROLE_USER', 'LOCAL');
+
+
+CREATE TABLE `friend` (
+  `user_id` BIGINT NOT NULL,
+  `request_time` JSON NULL,
+  `request_list` JSON NULL,
+  `friend_list` JSON NULL,
+  `black_list` JSON NULL,
+  PRIMARY KEY (`user_id`),
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`user_id`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION);
+
+/* 오류나면 root계정으로 로그인 후
+set global log_bin_trust_function_creators=1; 실행해주기 */
+    
+DELIMITER $$
+    CREATE TRIGGER insert_friend
+    AFTER INSERT
+    ON user
+    FOR EACH ROW
+    
+BEGIN
+INSERT INTO friend(user_id)
+VALUES(new.user_id);
+END;
+
+DELIMITER;
+
---
+++ src/backend/friend/src/main/java/com/example/friend/Entity/Friend.java
@@ -0,0 +1,21 @@
+package com.example.friend.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name="`friend`")
+public class Friend {
+
+    @Id
+    private User user;
+}
