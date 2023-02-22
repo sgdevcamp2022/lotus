54bf16697bd43f0a7f2106f3e197e36e162d7112
the-groot <sangwoo0795@naver.com>
2023-02-22 15:39:54 +0900

    fix: security requestmatchers 추가

--- src/backend/auth/src/main/java/com/example/auth/Config/SecurityConfig.java
+++ src/backend/auth/src/main/java/com/example/auth/Config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsUtils;
 
 @EnableWebSecurity
 @EnableMethodSecurity
@@ -88,6 +89,7 @@ public class SecurityConfig {
                 .and()
 
                 .authorizeRequests()
+                .requestMatchers(request -> CorsUtils.isPreFlightRequest(request)).permitAll()
                 .antMatchers(PERMIT_URL_ARRAY).permitAll()
                 .antMatchers("/test").hasRole("USER")
                 .anyRequest().authenticated()
