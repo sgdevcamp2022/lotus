b0811239c4a653b4dfdd7ed8eec34e1994ebdb6b
the-groot <sangwoo0795@naver.com>
2023-02-22 15:51:30 +0900

    fix: cors설정 추가

--- src/backend/auth/src/main/java/com/example/auth/Config/SecurityConfig.java
+++ src/backend/auth/src/main/java/com/example/auth/Config/SecurityConfig.java
@@ -79,6 +79,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
 
+                .cors().and()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
